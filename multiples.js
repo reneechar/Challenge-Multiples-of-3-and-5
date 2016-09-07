279da077cedd115408a108be4d777e3e6bf157a9
Renee <rchar20@gmail.com>
2016-09-07 10:43:00 -1000

    Completed multiples.js file

--- multiples.js
+++ multiples.js
@@ -3,10 +3,20 @@
  * @param  {Number} n Number that is to be used for calculating multiples up to that Number
  * @return {Number}
  */
+
 exports.sumOfAMultiple = function( n ) {
   var sum = 0;
+  var counter = 3;
+  while (counter < n) {
+  	if (counter%3 === 0) {
+  		sum += counter;
+  	} else if (counter%5 ===0) {
+  		sum += counter;
+  	}
+    counter++
+  }
 
-  // do your work here
-
+  console.log(sum);
   return sum;
 };
+
